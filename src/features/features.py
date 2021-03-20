dc77d7b0b0e36aa9e157aeeb4c64d017bfcd39cf
Lily Chau <chauyeeki@gmail.com>
2021-03-20 21:01:34 +0000

    updated feature.py to include original script from preprocessing.py

--- src/features/features.py
+++ src/features/features.py
@@ -1,3 +1,18 @@
 """
 - Create time feature
 """
+
+def create_time_features(meter_df):
+    # Create new feature "month"
+    meter_df["Month"] = meter_df["DateTime"].dt.month
+
+    # Create new feature "day"
+    meter_df["Day"] = meter_df["DateTime"].dt.day
+
+    # Create new feature "weekday"
+    meter_df["Weekday"] = meter_df["DateTime"].dt.weekday
+
+    # Create new feature "hour"
+    meter_df["Hour"] = meter_df["DateTime"].dt.hour
+
+    return meter_df
